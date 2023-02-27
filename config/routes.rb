0d44822d44b6c2cc7a18be7cf5e247fcd9ff13c9
Matheus Moura <matheusmoura2110@gmail.com>
2023-02-27 20:14:01 -0300

    push part of the project

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :users
+  root "movies#index"
+
+  resources :movies do
+    resources :reviews
+  end
+
+  resource :session, only: [:new, :create, :destroy]
+  get "signup" => "users#new"
+end
